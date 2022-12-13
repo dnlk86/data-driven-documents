ea1601882bcf5b40b917e1b731847cb80c30a5c2
Daniel Keicher <dnlkch86@gmail.com>
2022-12-13 15:20:41 +0100

    added useWindowSize hook for dynamic resizing

--- src/pages/bar-chart/BarChart.js
+++ src/pages/bar-chart/BarChart.js
@@ -2,28 +2,27 @@ import React, { useEffect, useRef } from "react";
 import styles from "./BarChart.module.css";
 
 import * as d3 from "d3";
+import { useWindowSize } from "../../hooks/useWindowSize.js";
 
 export function BarChart() {
     const ref = useRef();
+    const size = useWindowSize();
+
     useEffect(() => {
         const svg = d3.select(ref.current);
 
+        const h = size.height * 0.8;
+        const w = size.width * 0.9;
+
         svg.style("background-color", "var(--color-3)")
-            .style("height", "80vh")
-            .style("width", "90vw");
-    }, ref);
+            .style("height", `${h}px`)
+            .style("width", `${w}px`);
+    }, [ref, size]);
 
     return (
         <div className={styles.barchart}>
             <h2 id="title">Bar Chart</h2>
-            <svg
-                ref={ref}
-                // style={{
-                //     height: "80vh",
-                //     width: "90vw",
-                //     backgroundColor: "var(--color-3)",
-                // }}
-            >
+            <svg ref={ref}>
                 <g className="plot-area" />
                 <g id="x-axis" />
                 <g id="y-axis" />
