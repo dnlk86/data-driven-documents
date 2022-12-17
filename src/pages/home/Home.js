703d924df0f35706ce075cf1151c390c49f506d8
Daniel Keicher <dnlkch86@gmail.com>
2022-12-17 12:32:13 +0100

    improved home page content and style

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,19 +16,32 @@ export function Home() {
             </h2>
             <br />
             <p>
-                I used React.js to build this collection, together with the
-                react-router package, in order to have a better separation
-                between different projects.
+                I developed this project as a collection of d3 charts for the
+                relative section of the freeCodeCamp curriculum.
+                <br />I used React.js together with the react-router package to
+                build it, in order to have a better separation between the
+                different charts.
             </p>
             <br />
             <h3>List of technologies:</h3>
             <br />
             <ul>
                 <li>react.js using create-react-app</li>
-                <li>react-router-dom</li>
-                <li>d3.js</li>
+                <li>react-router-dom for routing</li>
+                <li>d3.js for all charts</li>
+                <li>axios for API requests</li>
                 <li>gh-pages for deployment</li>
             </ul>
+            <br />
+            <h3>A sidenote.</h3>
+            <br />
+            <p>
+                The way I decided to structure the project and the technologies
+                I used might seem a little overkill. That was exactly the goal I
+                set for this project, I wanted to test the d3.js functionalities
+                and at the same time see how all those technologies fit together
+                in a relatively simple but fully functioning case study.
+            </p>
         </div>
     );
 }
