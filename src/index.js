83673e99e769a9d3052e40506c9a24391ce95fd9
Daniel Keicher <dnlkch86@gmail.com>
2022-12-17 11:58:03 +0100

    fixed routing issue using hashRouter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 console.log(process.env);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Router basename="/data-driven-documents">
+        <HashRouter>
             <App />
-        </Router>
+        </HashRouter>
     </React.StrictMode>
 );
