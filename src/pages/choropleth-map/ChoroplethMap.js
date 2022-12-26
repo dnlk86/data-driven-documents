0efa7cdd68ed17307e2159bfcfb7a9263cf41e94
Daniel Keicher <dnlkch86@gmail.com>
2022-12-26 13:47:06 +0100

    useWindowSize hook causing double rendering

--- src/pages/choropleth-map/ChoroplethMap.js
+++ src/pages/choropleth-map/ChoroplethMap.js
@@ -2,12 +2,12 @@ import React, { useEffect, useRef } from "react";
 import styles from "./ChoroplethMap.module.css";
 
 import * as d3 from "d3";
-import { useWindowSize } from "../../hooks/useWindowSize.js";
+// import { useWindowSize } from "../../hooks/useWindowSize.js";
 // import { useFetch } from "../../hooks/useFetch.js";
 
 export function ChoroplethMap() {
     const ref = useRef();
-    const size = useWindowSize();
+    // const size = useWindowSize();
     // const { countyData, countyLoading } = useFetch(
     //     "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
     // );
@@ -18,8 +18,10 @@ export function ChoroplethMap() {
     useEffect(() => {
         const svg = d3.select(ref.current);
 
-        const h = Math.floor(size.height * 0.8);
-        const w = Math.floor(size.width * 0.9);
+        const h = window.innerHeight * 0.8;
+        const w = window.innerWidth * 0.9;
+        // const h = Math.floor(size.height * 0.8);
+        // const w = Math.floor(size.width * 0.9);
         const padding = 100;
 
         svg.style("background-color", "var(--color-1)")
@@ -31,14 +33,12 @@ export function ChoroplethMap() {
                 "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
             const eduUrl =
                 "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
-
             const responses = await Promise.all([
                 fetch(countyUrl),
                 fetch(eduUrl),
             ]);
             const countyData = await responses[0].json();
             const eduData = await responses[1].json();
-
             return [countyData, eduData];
         };
 
@@ -50,38 +50,41 @@ export function ChoroplethMap() {
 
             const tooltip = d3.select("#tooltip");
 
+            console.log(h);
+            console.log(w);
+
             // choropleth-title
-            svg.append("text")
+            svg.select("#title")
+                .append("text")
                 .text("United States Educational Attainment")
-                .attr("id", "title")
-                .attr("class", "labels")
                 .attr("text-anchor", "middle")
-                // .attr("x", w / 2)
+                .attr("x", w / 2)
                 .attr("y", 30)
                 .attr("fill", "var(--color-5)")
                 .style("font-size", "1.5rem")
                 .style("text-decoration", "underline");
 
             // choropleth-description
-            svg.append("text")
+            svg.select("#description")
+                .append("text")
                 .text(
                     "Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)"
                 )
-                .attr("id", "description")
-                .attr("class", "labels")
                 .attr("text-anchor", "middle")
-                // .attr("x", w / 2)
+                .attr("x", w / 2)
                 .attr("y", 60)
                 .attr("fill", "var(--color-5)")
                 .style("font-size", "1.0rem")
                 .style("text-decoration", "underline");
         });
-    }, [ref, size]);
+    }, [ref]);
 
     return (
         <div className={styles.choroplethMap}>
             <h2 id="page-title">d3 choropleth map example</h2>
             <svg ref={ref}>
+                <g id="title"></g>
+                <g id="description"></g>
                 <g id="x-axis" />
                 <g id="y-axis" />
                 <g id="legend">
